8b913be4e3da2444fe7c1995c958940fe93cb6dc
Pedro Luis <pedroluis200@nauta.com.cu>
2023-10-08 02:02:07 -0400

    Modify urls.py

--- rss/rss/urls.py
+++ rss/rss/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from rss_api.views import RSSFeedListCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rssapp.urls')),  # Incluye las URLs de tu aplicación rssapp
+    path('api/', include('rss_api.urls')),
+    path('', RSSFeedListCreateView.as_view(), name='home'),  # Vista para la ruta raíz
 ]
+
